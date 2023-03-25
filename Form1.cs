ea4d38f7f114e403169c37c982afb6eaa3653206
Tom Pivin <tommy.pivin94@gmail.com>
2023-03-24 21:24:46 -0400

    learning how to work with flow layout panel

--- Form1.cs
+++ Form1.cs
@@ -11,14 +11,27 @@ public partial class Form1 : Form
         flowLayoutPanel1.Dock = DockStyle.Fill;
 
         // Add the FlowLayoutPanel to the form's Controls collection
-        this.Constrols.Add(flowLayoutPanel1);
+        this.Controls.Add(flowLayoutPanel1);
 
         // Add buttons to The FlowLayoutPanel
-        for (int i = 0; i < 24; i++)
+        string[] buttonLabels = {"7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+" };
+        foreach( string label in buttonLabels)
         {
             Button button = new Button();
-            button.Text = "Button " + i;
-            buttonPanel.Controls.Add(button);
+            button.Text = label;
+            button.Font = new Font("Arial", 18, FontStyle.Bold);
+            button.Width = 70;
+            button.Height = 70;
+            button.Margin = new Padding(5);
+            button.Click += Button_Click;
+            flowLayoutPanel1.Controls.Add(button);
         }
     }
-}
+
+    private void Button_Click(object sender, EventArgs e)
+    {
+        
+    }
+
+}       
+
